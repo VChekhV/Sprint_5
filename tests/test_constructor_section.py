a534acca9fe0c7590acb1f4ae2984e3c1f6efa49
VChekhV <V.Chekh.V@yandex.ru>
2025-03-10 08:43:25 +1100

    Update test_constructor_section.py

--- tests/test_constructor_section.py
+++ tests/test_constructor_section.py
@@ -10,14 +10,14 @@ class TestConstructorPage:
         bun_class = driver.find_element(*MainPageLocators.ban_class)
         assert "current" in bun_class.get_attribute("class")
 
-        def test_transition_to_sauces_success(self, driver):
+    def test_transition_to_sauces_success(self, driver):
             # Проверка перехода к разделу 'Соусы'
             driver.get(URLS.MAIN_PAGE_URL)
             driver.find_element(*MainPageLocators.sauces_btn).click()
             sauces_class = driver.find_element(*MainPageLocators.sauces_class)
             assert "current" in sauces_class.get_attribute("class")
 
-        def test_transition_to_topping_success(self, driver):
+    def test_transition_to_topping_success(self, driver):
             # Проверка перехода к разделу 'Начинки'
             driver.get(URLS.MAIN_PAGE_URL)
             driver.find_element(*MainPageLocators.toppings_btn).click()
